0e7c55ae962a9710c0c22354122b1ae26897294e
Manuel Ortega Carcamo <mortegac@users.noreply.github.com>
2021-02-17 14:53:28 -0300

    Update home.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,239 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container">
+		<div className="row">
+			<div className="row flex-row flex-nowrap overflow-auto">
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+				<div className="card-deck">
+					<div className="card col-md-10 " style={{ width: "25rem" }}>
+						<img
+							src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=eb6fe33e-2fd2-4d71-8e7f-667b3072b669"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+							<i className="far fa-heart" />
+						</div>
+					</div>
+				</div>
+
+				{/* <div className="card" style={{ width: "18rem" }}>
+					<img
+						src="https://www.google.com/search?q=img+as+a+card&rlz=1C1VDKB_enCR939CR939&sxsrf=ALeKk02Iw1b_2Bl2hb5K2ZVsqS5MGAkUug:1613581106794&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjJ476FsvHuAhVWTTABHSlbCm4Q_AUoAXoECAcQAw&biw=1366&bih=638#imgrc=Jjq5a5o5G80fpM"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text" />
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+						<i className="far fa-heart" />
+					</div>
+				</div>
+				<div className="card" style={{ width: "18rem" }}>
+					<img
+						src="https://aquamarine-sheep-iccoswxq.ws-us03.gitpod.io/files/download/?id=59749433-5532-47b9-8dfa-b1edaf71090e"
+						className="card-img-top"
+						alt="..."
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text" />
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+						<i className="far fa-heart" />
+					</div>
+				</div>
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text" />
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+						<i className="far fa-heart" />
+					</div>
+				</div>
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text" />
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+						<i className="far fa-heart" />
+					</div>
+				</div>
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text" />
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+						<i className="far fa-heart" />
+					</div>
+				</div>
+
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text" />
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+						<i className="far fa-heart" />
+					</div>
+				</div> */}
+			</div>
+		</div>
 	</div>
 );
